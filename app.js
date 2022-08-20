1ab678fbced9cf89d2132562eb6afb647013a38f
eduardo-nakamura <edu-nakamura@hotmail.com>
2022-08-20 06:31:34 -0300

    menu header

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-console.log("Rodando 1")
-console.log("Rodando 2")
-console.log("Rodando 3")
+function openMenu() {
+    document.getElementById("container-nav").classList.add('ativa'); 
+}
+
+document.getElementById('container-nav').onclick = function(e) {    
+    if(e.target != document.getElementById('list-nav')) {    
+        document.getElementById("container-nav").classList.remove('ativa');       
+    } 
+}
