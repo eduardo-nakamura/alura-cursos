2296a0e14fff22cbf2bafe370a57aa82cdfc51bb
eduardo-nakamura <edu-nakamura@hotmail.com>
2022-08-31 10:10:39 -0300

    mongodb + mongoose setting

---
+++ courses-content/node-js-express/3-api-rest/config/dbConnect.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose"
+
+mongoose.connect("mongodb+srv://admin:T4odUsmY6pfF0LT6@cluster0.ykdsa9u.mongodb.net/?retryWrites=true&w=majority");
+
+let db = mongoose.connection
+
+export default db;
--- courses-content/node-js-express/3-api-rest/src/app.js
+++ courses-content/node-js-express/3-api-rest/src/app.js
@@ -1,4 +1,10 @@
 import express from "express"
+import db from "../config/dbConnect.js"
+
+db.on("error", console.log.bind(console, 'Database Connection Error'))
+db.once("open", () => {
+    console.log('Database Connection Success')
+})
 
 const app = express()
 
