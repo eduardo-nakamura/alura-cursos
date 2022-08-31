ce106e3ec94f7b451a3b4d2a6229a6fccea9408a
eduardo-nakamura <edu-nakamura@hotmail.com>
2022-08-31 08:07:08 -0300

    setting routes, install nodemon

--- courses-content/node-js-express/3-api-rest/1-starting-server.js
+++ courses-content/node-js-express/3-api-rest/1-starting-server.js
@@ -1,17 +1,9 @@
 const http = require('http')
 const port = 3000
 
-const routes = {
-    '/': 'Node Class',
-    '/books': 'Books Pages',
-    '/authors': 'Authors List',
-    '/publishers': 'Publishers List',
-    '/about': 'About Project',
-}
-
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'})
-    res.end(routes[req.url])
+    res.end('Node Class')
 })
 
 server.listen(port, () => {
